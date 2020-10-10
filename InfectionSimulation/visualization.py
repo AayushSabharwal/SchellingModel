c0749be122102a9d57d03da24dd2e058bc17e4bb
Aayush Sabharwal <aayush.sabharwal@gmail.com>
2020-10-10 23:19:47 +0530

    Simulation changes, visualization changes
    Visualization now no longer has a grid, improving performance. Simulation
    parameters are real world covid values. Simulation now uses continuous space
    and perlin noise based movements

--- InfectionSimulation/visualization.py
+++ InfectionSimulation/visualization.py
@@ -1,7 +1,7 @@
-from mesa.visualization.modules import CanvasGrid, ChartModule
+from mesa.visualization.modules import ChartModule
 from mesa.visualization.ModularVisualization import ModularServer
 from core_simulation import InfectionModel
-from simulation_parameters import grid_size, num_agents
+from simulation_parameters import grid_size, num_agents, initial_infected_chance
 from utility import InfectionState
 
 
@@ -25,8 +25,6 @@ def agent_portrayal(agent):
     return portrayal
 
 
-# visual grid on which agents move
-grid = CanvasGrid(agent_portrayal, grid_size[0], grid_size[1], 500, 500)
 # chart that plots the number of agents in each state with time
 chart1 = ChartModule([{"Label": "infected", "Color": "green"},
                       {"Label": "recovered", "Color": "blue"},
@@ -37,11 +35,11 @@ chart2 = ChartModule([{"Label": "total_infections", "Color": "green"},
                       {"Label": "total_recoveries", "Color": "blue"},
                       {"Label": "deaths", "Color": "grey"}])
 # just mesa stuff
-server = ModularServer(InfectionModel, [grid, chart1, chart2], "Infection Model",
+server = ModularServer(InfectionModel, [chart1, chart2], "Infection Model",
                        {
                        "num_agents": num_agents,
                        "grid_size": grid_size,
-                       "initial_infected_chance": 0.05
+                       "initial_infected_chance": initial_infected_chance
                        })
 server.port = 8521
 server.launch()
